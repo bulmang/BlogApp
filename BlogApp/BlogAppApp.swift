a3f84623d75a9ffaa0969c248a89fbc9560822d7
Bulmang <hmk113737@gmail.com>
2022-10-13 23:13:35 +0900

    Initial Commit

---
+++ BlogApp/BlogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogAppApp.swift
+//  BlogApp
+//
+//  Created by 하명관 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct BlogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
